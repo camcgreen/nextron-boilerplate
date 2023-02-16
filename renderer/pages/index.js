5430e46a546a00f1ec566aebc1e98987c77b87fd
cameron-green-1 <87066027+cameron-green-1@users.noreply.github.com>
2023-02-16 09:56:48 +0000

    Add intro text

--- renderer/pages/index.js
+++ renderer/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     }, []);
     return (
         <div className={styles.wrapper}>
-            <h1>Welcome to Nextron Boilerplate! Helloooo</h1>
+            <h1>Welcome to Nextron Boilerplate!</h1>
         </div>
     );
 }
